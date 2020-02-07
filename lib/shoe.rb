250571899617306f6035d7f2ee560e73aac3655e
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-07 00:54:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS |= brand
-    
+    BRANDS << brand
+    BRANDS.push(brand) unless BRANDS.include?(brand) 
   end
 
   def cobble
